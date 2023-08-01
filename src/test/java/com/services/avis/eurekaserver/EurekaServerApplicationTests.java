660a9deb12187f7ca9ecfbfdb7be732932b254c6
siva.gangadhar <siva.gangadhar@litmus7.com>
2023-08-01 12:28:54 +0530

    first commit

---
+++ src/test/java/com/services/avis/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.avis.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
